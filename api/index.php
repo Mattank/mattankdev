792c2f74f5d3de2d561b952acb9c7ccac96af062
Matheus Freitas <matheus-d-freitas@openlabs.com.br>
2022-09-28 16:00:35 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -1,27 +1,63 @@
 <?php
 
-spl_autoload_register(function($nome_arquivo)
-{
-    if(file_exists('Controllers/'.$nome_arquivo.'.php'))
-    {
-        require 'Controllers/'.$nome_arquivo.'.php';
+require 'autoload.php';
 
-    }elseif (file_exists('Core/'.$nome_arquivo.'.php'))
+//
+Class Core{
+    public function __construct()
     {
-        require 'Core/'.$nome_arquivo.'.php';
-
-    }elseif(file_exists('Models/'.$nome_arquivo.'Model.php'))
-    {
-        require 'Models/'.$nome_arquivo.'Model.php';
-
-    }elseif(file_exists('Views/'.$nome_arquivo.'.php'))
-    {
-        require 'Views/'.$nome_arquivo.'.php';
+        $this->run();
     }
 
-});
+    public function run()
+    {
+        //Verifica se foi passado algo no dominio e salva na variavel url
+        if(isset($_GET['pag']))
+        {
+            $url = $_GET['pag'];
+        }
+        //Verifica é a url não está vazia e separa Controlador, Metodo e parametro
+        if(empty($url)){
+            $controller = 'homeController';
+            $metodo = 'index';
+            $parametros = array();
+        }
+        if(!empty($url)) 
+        {
+            $url = explode('/',$url);
+            $controller = $url[0].'Controller';
+            array_shift($url);
+            
+            if(isset($url[0]) && !empty($url[0]))
+            {
+                $metodo = $url[0];
+                array_shift($url);
+            }else
+            {
+                $metodo = 'index';
+            }
+            if(count($url) > 0)
+            {
+                $parametros = $url;
+            }else{
+                $parametros = $url;
+            }
+        }
+        
+        // Procura o Controller passado e executa o seu metodo
+        $caminho = 'Controllers/'.$controller.'.php';
 
-    require 'Core/core.php';
+        if(!file_exists($caminho) && !method_exists($controller, $metodo))
+        {
+            $controller = 'homeController';
+            $metodo = 'index';
+        }
+            
+        
+        $c = new $controller;
+        call_user_func_array(array($c,$metodo), $parametros);
+    }
+}
 
 $c = new Core();
 
