8f90acf13bae9a0a521bccb434b3e84a406d7dc8
Matheus Freitas <matheus-d-freitas@openlabs.com.br>
2022-09-28 16:15:07 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -1,83 +1,15 @@
 <?php
-
-spl_autoload_register(function($nome_arquivo)
-{
-    if(file_exists('Controllers/'.$nome_arquivo.'.php'))
-    {
-        require 'Controllers/'.$nome_arquivo.'.php';
-
-    }elseif (file_exists('Core/'.$nome_arquivo.'.php'))
-    {
-        require 'Core/'.$nome_arquivo.'.php';
-
-    }elseif(file_exists('Models/'.$nome_arquivo.'Model.php'))
-    {
-        require 'Models/'.$nome_arquivo.'Model.php';
-
-    }elseif(file_exists('Views/'.$nome_arquivo.'.php'))
-    {
-        require 'Views/'.$nome_arquivo.'.php';
-    }
-
-});
+//Core Load
+require 'Core/Controller.php';
+require 'Core/Core.php';
+//Controllers Load
 require 'Controllers/homeController.php';
 
-//
-Class Core{
-    public function __construct()
-    {
-        $this->run();
-    }
-
-    public function run()
-    {
-        //Verifica se foi passado algo no dominio e salva na variavel url
-        if(isset($_GET['pag']))
-        {
-            $url = $_GET['pag'];
-        }
-        //Verifica é a url não está vazia e separa Controlador, Metodo e parametro
-        if(empty($url)){
-            $controller = 'homeController';
-            $metodo = 'index';
-            $parametros = array();
-        }
-        if(!empty($url)) 
-        {
-            $url = explode('/',$url);
-            $controller = $url[0].'Controller';
-            array_shift($url);
-            
-            if(isset($url[0]) && !empty($url[0]))
-            {
-                $metodo = $url[0];
-                array_shift($url);
-            }else
-            {
-                $metodo = 'index';
-            }
-            if(count($url) > 0)
-            {
-                $parametros = $url;
-            }else{
-                $parametros = $url;
-            }
-        }
-        
-        // Procura o Controller passado e executa o seu metodo
-        $caminho = 'Controllers/'.$controller.'.php';
-
-        if(!file_exists($caminho) && !method_exists($controller, $metodo))
-        {
-            $controller = 'homeController';
-            $metodo = 'index';
-        }
-            
-        
-        $c = new $controller;
-        call_user_func_array(array($c,$metodo), $parametros);
-    }
-}
+//Model Load
+require 'Models/conexao.php';
+require 'Models/home.php';
+//Views Load
+require 'Views/home.php';
 
 $c = new Core();
 
