8d607543d11d2cfe7e1c9eee370af4aea72abf11
Matheus Freitas <matheus-d-freitas@openlabs.com.br>
2022-09-28 15:36:50 -0300

    e

--- api/autoload.php
+++ api/autoload.php
@@ -2,21 +2,21 @@
 
 spl_autoload_register(function($nome_arquivo)
 {
-    if(file_exists('Controllers/'.$nome_arquivo.'.php'))
+    if(file_exists('api/Controllers/'.$nome_arquivo.'.php'))
     {
-        require 'Controllers/'.$nome_arquivo.'.php';
+        require 'api/Controllers/'.$nome_arquivo.'.php';
 
-    }elseif (file_exists('Core/'.$nome_arquivo.'.php'))
+    }elseif (file_exists('api/Core/'.$nome_arquivo.'.php'))
     {
-        require 'Core/'.$nome_arquivo.'.php';
+        require 'api/Core/'.$nome_arquivo.'.php';
 
-    }elseif(file_exists('Models/'.$nome_arquivo.'Model.php'))
+    }elseif(file_exists('/api/Models/'.$nome_arquivo.'Model.php'))
     {
-        require 'Models/'.$nome_arquivo.'Model.php';
+        require 'api/Models/'.$nome_arquivo.'Model.php';
 
-    }elseif(file_exists('Views/'.$nome_arquivo.'.php'))
+    }elseif(file_exists('api/Views/'.$nome_arquivo.'.php'))
     {
-        require 'Views/'.$nome_arquivo.'.php';
+        require 'api/Views/'.$nome_arquivo.'.php';
     }
 
 });
